7db85a3de77f410634e0a8a401459770d5d9e1e1
udit7395 <udit7395@gmail.com>
2017-11-22 15:42:41 +0530

    Replace Legacy usage && Double quote to prevent globbing and word splitting

--- getDataWithoutRoot.sh
+++ getDataWithoutRoot.sh
@@ -12,22 +12,21 @@ if [ -z "$1" ]
     echo "No packagename supplied"
     echo "Input as ----> ./getDataWithoutRoot.sh <packagename>"
 else
- 	file_name=data.ab
-	path=`pwd`/$1_`(date +%d_%m_%Y_%H_%M_%S)`
-	mkdir $path
-	mkdir $path/databases
-	mkdir $path/sharedprefrences
+	path=$(pwd)/$1_$(date +%d_%m_%Y_%H_%M_%S)
+	mkdir "$path"
+	mkdir "$path/databases"
+	mkdir "$path/sharedprefrences"
 	mkdir backup
-	cd backup
+	cd backup || exit
 	echo "Do not enter any Password"
-	adb backup -f data.ab -noapk $1
+	adb backup -f data.ab -noapk "$1"
 	if [ -f data.ab ]; then
 	    echo "File found!"
 	fi
 	dd if=data.ab bs=1 skip=24 | python -c "import zlib,sys;sys.stdout.write(zlib.decompress(sys.stdin.read()))" | tar -xvf -
 	cd ..
-	echo $path
-	rsync --progress backup/apps/$1/db/*.db $path/databases
-	rsync --progress backup/apps/$1/sp/*.xml $path/sharedprefrences
+	echo "$path"
+	rsync --progress backup/apps/"$1"/db/*.db "$path"/databases
+	rsync --progress backup/apps/"$1"/sp/*.xml "$path"/sharedprefrences
 	rm -rf backup
 fi
